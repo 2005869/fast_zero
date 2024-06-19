5edb8c531f6e8376a4008f4ea85c6a6414c2c15d
2005869 <2005869@aluno.univesp.br>
2024-06-19 09:35:34 -0300

    Aula 02

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,17 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_root_dve_retornar_ok_e_ola_mundo():
+def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)
-
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo'}
+
+
+def test_deve_retornar_ok_e_html():
+    client = TestClient(app)
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == """<h1>Olá Mundo</h1>"""
