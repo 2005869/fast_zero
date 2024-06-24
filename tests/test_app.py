0aa8e8b07d0daf8ebcdbbc687f6be87707d468e6
2005869 <2005869@aluno.univesp.br>
2024-06-24 18:59:57 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -37,18 +39,17 @@ def test_read_users(client):
     response = client.get('/users')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@exemplo.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users')
+
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -57,7 +58,6 @@ def test_update_user(client):
             'password': 'mynewpassword',
         },
     )
-
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'username': 'bob',
@@ -94,6 +94,13 @@ def test_update_user_not_found_menor_que_database(client):
     assert response.json() == {'detail': 'User not found'}
 
 
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
 def test_get_user(client):
     response = client.get('/users/1')
 
@@ -124,13 +131,6 @@ def test_delete_user_not_found_menor_que_database(client):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
-
-
-def test_delete_user(client):
-    response = client.delete('/users/1')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'User deleted'}
 
 
 def test_delete_user_not_exist(client):
